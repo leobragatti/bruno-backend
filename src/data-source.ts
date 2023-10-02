efa769fbefa0cb4adbced2aa129e05a9836e466b
Leonardo Bragatti <leonardo.bragatti@gmail.com>
2023-10-02 16:50:34 -0300

    Adding environment variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,27 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { User } from "./entity/User";
 import { Tasks } from "./entity/Tasks";
+import "dotenv/config";
+
+const getDBConfig = (json: any) => {
+  const newObj = {};
+
+  for (const prop in json) {
+    if (!prop.startsWith("DB_")) {
+      continue;
+    }
+    const newProp = prop.substring(prop.indexOf("_") + 1).toLowerCase();
+    newObj[newProp] = json[prop];
+  }
+
+  return newObj;
+};
+
+const config = getDBConfig(process.env) as DataSourceOptions;
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "mahmud.db.elephantsql.com",
-  port: 5432,
-  username: "nwwaukfs",
-  password: "RfoAC1pexWS_hcVdCqPFuJEiQhFkczBD",
-  database: "nwwaukfs",
+  ...config,
   synchronize: true,
   logging: false,
   entities: [User, Tasks],
